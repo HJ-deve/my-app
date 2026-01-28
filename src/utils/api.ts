7719e222c99caeaf4d6272c44c034002ee90810e
HJ <hunjoolee@HUNJOO-M4-MacBook-Pro.local>
2026-01-28 10:21:18 +0900

    axios

---
+++ src/utils/api.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+export const jsonPlaceholderRequest = axios.create({
+  baseURL: 'http://jsonplacehodler.typicode.com/',
+  withCredentials: false,
+  timeout: 3000,
+});
+
+jsonPlaceholderRequest.interceptors.request.use(
+  (config) => {
+    console.log('호출 전 수행할 작업');
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+jsonPlaceholderRequest.interceptors.request.use(
+  (response) => {
+    console.log('응답 데이터를 가공하여 반환');
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
