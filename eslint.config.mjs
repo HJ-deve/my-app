c20d9d8cb87baa087cbafde091bac92dc20f1da1
HJ <hunjoolee@HUNJOO-M4-MacBook-Pro.local>
2026-02-03 00:13:05 +0900

    cdd(without index.stories.tsx)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,19 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook';
+
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
     languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
@@ -18,19 +21,21 @@ export default defineConfig([
   {
     plugins: {
       react,
-      "react-hooks": reactHooks,
+      'react-hooks': reactHooks,
     },
 
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
 
     rules: {
-      "react/react-in-jsx-scope": "off",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/no-unescaped-entities': 0,
     },
   },
+  ...storybook.configs['flat/recommended'],
 ]);
